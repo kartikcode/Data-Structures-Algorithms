2e94acf42257f6d59e2e36d6dc8b936311ba2b0f
Kartikeya Gupta <gkartik@iitk.ac.in>
2020-09-01 21:46:04 +0530

    Added helper functions

--- elementary_sorts/Date.java
+++ elementary_sorts/Date.java
@@ -15,4 +15,27 @@ public class Date implements Comparable<Date>{
         if(that.date<this.date) return 1;
         return 0;
     }
+
+    /*
+     * Helper Functions
+     */
+    
+    private static boolean less (Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean isSorted(Comparable a[]){
+        for(int i=1;i<a.length;i++){
+            if(less(a[i],a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
